e0d505c1ef5ee0005f50fb4896dbb3d671720910
Joao Barreto <eng.joao.barreto@gmail.com>
2019-12-09 09:56:15 +0000

    init maven project

---
+++ src/main/java/com/criticaltechworks/upacademytreinos/UpacademytreinosApplication.java
@@ -0,0 +1,13 @@
+package com.criticaltechworks.upacademytreinos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpacademytreinosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpacademytreinosApplication.class, args);
+	}
+
+}
