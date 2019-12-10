f4b8f2ab03f03de029b149e468c4cbc77a6ad2b2
Joao Barreto <eng.joao.barreto@gmail.com>
2019-12-10 13:23:03 +0000

    starting model

--- src/main/java/model/Treino.java
+++ src/main/java/model/Treino.java
@@ -1,5 +1,27 @@
 package model;
 
+import java.sql.Date;
+import java.util.Collection;
+import java.util.HashSet;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Treino {
 
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String titulo;
+	private String local;
+	private Date duracao;
+	private int lotacao;
+	private Collection<String> preRequisitos;
+	private long formadorId;
 }
--- src/main/java/model/Utilizador.java
+++ src/main/java/model/Utilizador.java
@@ -1,8 +1,50 @@
 package model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Utilizador {
 
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String nome;
+	private String email;
+	private String password;
+	private String role;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
 }
